49beadc24b0c3a8d9281b6339226c20951d137c4
jinkyungkim12 <skwlsrud3@naver.com>
2022-12-28 16:30:05 +0900

    up

---
+++ src/router/routes.js
@@ -0,0 +1,21 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'typography', component: () => import('pages/Typography.vue') },
+      { path: 'table', component: () => import('pages/Table.vue') },
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
